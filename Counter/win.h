c1c641c95c1e6856478fa363e661b813b89030c5
Basti <basti.babaeva@yandex.ru>
2024-04-01 11:56:50 +0700

    Изначальный код задания 2.8 с методички

--- Counter/win.h
+++ Counter/win.h
@@ -1,21 +1,35 @@
-#ifndef WIN_H
-#define WIN_H
-
-#include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class win; }
-QT_END_NAMESPACE
-
-class win : public QMainWindow
+#ifndef win_h
+#define win_h
+#include <QtGui>
+class Counter:public QLineEdit
 {
     Q_OBJECT
-
 public:
-    win(QWidget *parent = nullptr);
-    ~win();
-
-private:
-    Ui::win *ui;
+    Counter(const QString & contents, QWidget *parent=0):
+        QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str=text();
+        int r=str.toInt();
+        if (r!=0 && r%5 ==0) emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
 };
-#endif // WIN_H
+class Win: public QWidget
+{
+    Q_OBJECT
+protected:
+    QTextCodec *codec;
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
+public:
+    Win(QWidget *parent = 0);
+};
+#endif

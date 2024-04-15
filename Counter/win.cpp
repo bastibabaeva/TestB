52c0624f19dacb9c0f9be97a852cce5822bd196c
Basti <basti.babaeva@yandex.ru>
2024-04-15 14:36:54 +0700

    Редактирование сигнально-слотовых связей

--- Counter/win.cpp
+++ Counter/win.cpp
@@ -31,10 +31,7 @@ Win::Win(QWidget *parent):QWidget(parent)
     layout4->addLayout(layout3);
 
     // связь сигнала нажатия кнопки и слота закрытия окна
-    connect(calcbutton,SIGNAL(clicked(bool)),
-            edit1,SLOT(add_one()));
-    connect(edit1,SIGNAL(tick_signal()),
-            edit2,SLOT(add_one()));
-    connect(exitbutton,SIGNAL(clicked(bool)),
-            this,SLOT(close()));
+    connect(calcbutton, SIGNAL(clicked(bool)), edit1, SLOT(add_one()));
+    connect(edit1, SIGNAL(tick_signal()), edit2, SLOT(add_one()));
+    connect(exitbutton, SIGNAL(clicked(bool)), this, SLOT(close()));
 }

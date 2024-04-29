fca73ab5333a87414d7ad824013f2edee33c269d
bastibabaeva <basti.babaeva@yandex.ru>
2024-04-30 00:19:55 +0700

    Удаление кодека

--- Degree2Counter/win.h
+++ Degree2Counter/win.h
@@ -32,7 +32,7 @@ public:
     StrValidator(QObject *parent):QValidator(parent){}
     virtual State validate(QString &str,int &pos)const
     {
-        qDebug()<<str<<endl;
+        qDebug()<<str<<endl;//вывод сообщения для отладки
         return Acceptable; // метод всегда принимает вводимую строку
     }
 };
